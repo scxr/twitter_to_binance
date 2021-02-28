9a383657ae481e12e2bd691ad3dcd72ffc24a486
scxr <cswilson@zoho.com>
2021-02-28 22:32:19 +0000

    updated handle transactions

--- handle_transactions.py
+++ handle_transactions.py
@@ -25,7 +25,15 @@ def mark_closed(order_id, sell_price):
 
 def get_transaction(order_id):
     transaction = session.query(Transactions).filter_by(order_id=order_id).first()
-    print(transaction)
+    return transaction
+
+def calculate_returns():
+    total_buys = session.query(Transactions).filter(Transactions.buy_price).all()
+    total_sells = session.query(Transactions).filter(Transactions.buy_price).all()
+    buys_cost = sum([i.buy_price * i.amount  for i in total_buys if not i.still_open ])
+    sells_cost = sum([i.sell_price * i.amount for i in total_sells])
+    return f'Total spent on buying : ${buys_cost}, Total sold : ${sells_cost}, Profit : ${sells_cost - buys_cost}'
+print(calculate_returns())
 #add_new_transaction('d',1,1,'111')
 #mark_closed('111', 10)
 #get_transaction('111')
